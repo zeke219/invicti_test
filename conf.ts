2d224e0b13e3ac5550a772cf816210114521c0c1
zeke_welend <zeke.ng@welab.co>
2021-05-07 22:08:04 +0100

    update: update configs

--- conf.ts
+++ conf.ts
@@ -1,30 +1,29 @@
-import {Config, browser} from 'protractor';
+import { Config, browser } from "protractor";
 
 export let config: Config = {
-    //Check the "app-root" for angular applications
-    rootElement: 'app',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    allScriptsTimeout: 11000,
-    framework: 'custom',
-    frameworkPath: require.resolve('protractor-cucumber-framework'),
-    capabilities: {
-        browserName: 'chrome',
-        chromeOptions: {
-          args: ['incognito']
-        }
-      },
-    maxSessions: 1,
-    noGlobals: true,
-    onPrepare: () => {
-        browser.driver.manage().window().setSize(1400, 900);
+  //Check the "app-root" for angular applications
+  rootElement: "app",
+  seleniumAddress: "http://localhost:4444/wd/hub",
+  allScriptsTimeout: 11000,
+  framework: "custom",
+  frameworkPath: require.resolve("protractor-cucumber-framework"),
+  capabilities: {
+    browserName: "chrome",
+    chromeOptions: {
+      args: ["incognito"],
     },
-    // Base Url in SBX
-    baseUrl: 'http://demo.testfire.net/',
-    specs: ['../features/**/*.feature'],
-    cucumberOpts: {
-        require: 'step_definitions/*.js',
-        format: ['progress'],
-        'format-options':  '{"colorsEnabled": true}'
-    }
+  },
+  maxSessions: 1,
+  noGlobals: true,
+  onPrepare: async () => {
+    await browser.waitForAngularEnabled(false);
+    await browser.driver.manage().window().setSize(1400, 900);
+  },
+  baseUrl: "http://demo.testfire.net/",
+  specs: ["../features/**/*.feature"],
+  cucumberOpts: {
+    require: "step_definitions/*.js",
+    format: ["progress"],
+    "format-options": '{"colorsEnabled": true}',
+  },
 };
-
